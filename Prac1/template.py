e325bf0abc40e565f734c7d43fc8ba24834a4503
dmlr24 <dmlr24@outlook.com>
2019-08-07 21:38:28 +0100

    Final commit, implemented LED, input logic

--- Prac1/template.py
+++ Prac1/template.py
@@ -3,18 +3,89 @@
 Python Practical Template
 Keegan Crankshaw
 Readjust this Docstring as follows:
-Names: <names>
-Student Number: <studnum>
-Prac: <Prac Num>
-Date: <dd/mm/yyyy>
+Names: Dylan
+Student Number: MLLDYL002
+Prac: Practical 1
+Date: 7/21/2019
 """
 
 # import Relevant Librares
 import RPi.GPIO as GPIO
+import time
 
+# Define board pins
+# Inputs
+INPUT_0 = 15
+INPUT_1 = 36
+
+# Leds
+LED_0 = 7
+LED_1 = 11
+LED_2 = 13
+
+# Global count variable
+count = 0
+
+# Function to apply LED states using bitmask
+def writeLEDS(state):
+    GPIO.output(LED_0, count & 0x1)
+    GPIO.output(LED_1, count & 0x2)
+    GPIO.output(LED_2, count & 0x4)
+
+# Routine to handle decrementing count variable
+# Update LED states using writeLEDS
+def decrement(pin):
+    # Access global variable
+    global count
+    # Reset counter at extent
+    if(count <=0):
+        count = 0x7
+    else:
+        count = count - 1
+    # Update LED states
+    writeLEDS(count)
+
+# Routine to handle incrementing count variable
+# Update LED states using writeLEDS
+def increment(pin):
+    # Access global variable
+    global count
+    if(count >=0x7):
+        count = 0
+    else:
+        count = count + 1
+        
+    # Update LED states
+    writeLEDS(count)
+    
 # Logic that you write
 def main():
-    print("write your logic here")
+    # Define pin layout
+    GPIO.setmode(GPIO.BOARD)
+    
+    # Setup outputs
+    # Set GPIO to type OUTPUT
+    GPIO.setup(LED_0, GPIO.OUT)
+    GPIO.setup(LED_1, GPIO.OUT)
+    GPIO.setup(LED_2, GPIO.OUT)
+    
+    # Setup Inputs
+    # Set GPIO to type INPUT
+    # Enable pull-up resistors for both inputs
+    GPIO.setup(INPUT_0, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.setup(INPUT_1, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    
+    # Add interrupt handlers for both inputs
+    # Set debounce time
+    GPIO.add_event_detect(INPUT_0, GPIO.FALLING, callback=decrement, bouncetime=300 )
+    GPIO.add_event_detect(INPUT_1, GPIO.FALLING, callback=increment, bouncetime=300 )
+    
+    # Initialize LED to rest state (0x0)
+    writeLEDS(count)
+    
+    # Infinite loop, wait for events
+    while True:
+        time.sleep(5)
 
 
 # Only run the functions if 
